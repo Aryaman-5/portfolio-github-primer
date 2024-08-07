332ebde673ebb7a6bd7c169f9e53712fc24f61ff
Aryan Mollick <66239481+Aryaman-5@users.noreply.github.com>
2024-08-07 09:18:47 +0000

    Did some of the neccessary changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science Engineering student studying at University Institute of Technology. I enjoy creating unique and impactful projects through Web . I am passionate about creating inclusive designs that are accessible to all.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,9 +32,9 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Cyber Security",
+  "Artificial Intelligence",
+  "Data Structures",
   "Mobile user interfaces",
   "Graphic design",
 ];
